070e19ff1cb51978f032817d450665b1c0a89612
Manuel Alejandro Rodriguez Sanchez <manuel.rodriguez9120@alumnos.udg.mx>
2024-09-29 16:28:26 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     devServer:{
-        host: '0.0.0.0',
+        host: 'https://markdown-translator.onrender.com',
         disableHostCheck: true,
         port: 10000,
     }
